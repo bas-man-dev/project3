33cb0eb91f36843c7e62da2a48aa28904d8efa08
Bas Man <bas.man.developer@gmail.com>
2021-12-24 23:03:44 +0000

    using an object for the data pt 1

--- src/App.js
+++ src/App.js
@@ -3,15 +3,45 @@ import './App.css';
 import Employee from './Employee';
 
 
+
+
 function App() {
+  const employeeInfo = [
+    {
+      firstName: "John",
+      lastName:"Smith",
+      hobby: "Tennis",
+      id: 1
+    },
+    {
+      firstName: "Dave",
+      lastName:"Brown",
+      hobby: "Snooker",
+      id: 2
+    },
+    {
+      firstName: "Mary",
+      lastName:"James",
+      hobby: "Jogging",
+      id: 3
+    },
+    {
+      firstName: "Anna",
+      lastName:"Jones",
+      hobby: "Swimming",
+      id: 4
+    }
+  ]
   return (
     <div className="App">
       <header className="App-header">
       <h1>Company Directory</h1>
-      <Employee firstName="John" lastName="Smith" hobby="tennis" />
-      <Employee firstName="Dave" lastName="Brown"  hobby="archery" />
-      <Employee firstName="Mary" lastName="James"  hobby="bowling" />
-      <Employee firstName="Anne" lastName="Jones"  hobby="running" />
+      {employeeInfo.map(employee => {
+        const { firstName, lastName, hobby, id } = employee;
+        return (
+          <Employee key={id} firstName={firstName} lastName={lastName} hobby={hobby} />
+        )
+      })}
       </header>
     </div>
   );
